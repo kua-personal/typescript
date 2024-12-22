763c1c461d0a02ac672a1cde4f50c64f280cb46d
HoYoung <kgerust@gmail.com>
2024-12-22 23:34:39 +0900

    section11 react with ts

---
+++ section11/src/components/Editor.tsx
@@ -0,0 +1,30 @@
+import React, {useContext, useState} from "react";
+import {TodoDispatchContext, useTodoDispatch} from "../App";
+
+interface Props {
+}
+
+const Editor = (props: Props) => {
+    const dispatch = useTodoDispatch()
+
+    const [text, setText] = useState("")
+
+    const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setText(e.target.value)
+    }
+
+    const onClickButton = () => {
+        dispatch.onClickAdd(text)
+        setText("")
+    }
+
+    return <div>
+        <input
+            value={text}
+            onChange={onChangeInput}
+        />
+        <button onClick={onClickButton}>Add</button>
+    </div>
+}
+
+export default Editor
